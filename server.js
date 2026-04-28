cdd40ad03231de9ac41d30cb6b17dafeb5f2d1f8
stokes1982 <samuelstokes7@gmail.com>
2026-04-28 05:55:22 +0100

    Remove server port configuration and listener
    Removed server listening code.

--- server.js
+++ server.js
@@ -751,8 +751,4 @@ server.on("error", (error) => {
   throw error;
 });
 
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`App running on port ${PORT}`);
-});
